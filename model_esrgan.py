3c4a3cabf6b5584ee77951e8021586780a303e3c
KyungBong-Ryu <103981195+KyungBong-Ryu@users.noreply.github.com>
2022-05-28 17:04:31 +0900

    Update model_esrgan.py
    ContentLoss fix init

--- model_esrgan.py
+++ model_esrgan.py
@@ -263,7 +263,7 @@ class Generator(nn.Module):
                 if module.bias is not None:
                     nn.init.constant_(module.bias, 0)
 
-
+'''
 class ContentLoss(nn.Module):
     """Constructs a content loss function based on the VGG19 network.
     Using high-level feature mapping layers from the latter layers will focus more on the texture content of the image.
@@ -306,11 +306,56 @@ class ContentLoss(nn.Module):
         content_loss = F.l1_loss(sr_feature, hr_feature)
 
         return content_loss
-
+'''
 
 
 # <2> ***********************************************************************************************
 
+#fixed
+class ContentLoss(nn.Module):
+    """Constructs a content loss function based on the VGG19 network.
+    Using high-level feature mapping layers from the latter layers will focus more on the texture content of the image.
+    Paper reference list:
+        -`Photo-Realistic Single Image Super-Resolution Using a Generative Adversarial Network <https://arxiv.org/pdf/1609.04802.pdf>` paper.
+        -`ESRGAN: Enhanced Super-Resolution Generative Adversarial Networks                    <https://arxiv.org/pdf/1809.00219.pdf>` paper.
+        -`Perceptual Extreme Super Resolution Network with Receptive Field Block               <https://arxiv.org/pdf/2005.12597.pdf>` paper.
+     """
 
+    def __init__(self, feature_model_extractor_node: str,
+                 feature_model_normalize_mean: list,
+                 feature_model_normalize_std: list) -> None:
+        super(ContentLoss, self).__init__()
+        # Get the name of the specified feature extraction node
+        self.feature_model_extractor_node = feature_model_extractor_node
+        # Load the VGG19 model trained on the ImageNet dataset.
+        model = models.vgg19(True)
+        # Extract the thirty-fifth layer output in the VGG19 model as the content loss.
+        self.feature_extractor = create_feature_extractor(model, [feature_model_extractor_node])
+        # set to validation mode
+        self.feature_extractor.eval()
+
+        # The preprocessing method of the input data.
+        # This is the VGG model preprocessing method of the ImageNet dataset
+        self.normalize = transforms.Normalize(feature_model_normalize_mean, feature_model_normalize_std)
+
+        # Freeze model parameters.
+        for model_parameters in self.feature_extractor.parameters():
+            model_parameters.requires_grad = False
+
+    def forward(self, sr_tensor: torch.Tensor, hr_tensor: torch.Tensor, is_AMP: bool) -> torch.Tensor:
+        # Standardized operations
+        sr_tensor = self.normalize(sr_tensor)
+        hr_tensor = self.normalize(hr_tensor)
+        
+        #does loss calclation processed under "torch.cuda.amp.autocast(enabled=True):" effect?
+        print("is_AMP", is_AMP)
+        with torch.cuda.amp.autocast(enabled=is_AMP):
+            sr_feature = self.feature_extractor(sr_tensor)[self.feature_model_extractor_node]
+            hr_feature = self.feature_extractor(hr_tensor)[self.feature_model_extractor_node]
+
+            # Find the feature map difference between the two images
+            content_loss = F.l1_loss(sr_feature, hr_feature)
+
+            return content_loss
 
 print("End of model_esrgan.py")
